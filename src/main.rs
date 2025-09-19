7620b2b515a697244674063073e867512ab606c3
Dipzz-newbie <mnadhiftriyudo@gmail.com>
2025-09-19 18:58:53 +0700

    Udpate

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ mod vehicle;
 #[tokio::main]
 async fn main() {
     // create a router 
-    let router1 = Router::new().route("/vehicle", get(vehicle::vehicle_get).post(vehicle::vehicle_get_post));
+    let app = Router::new().route("/vehicle", get(vehicle::vehicle_get).post(vehicle::vehicle_get_post));
 
     //2. define the address and port tcp
     let addres = "localhost:8888";
     let listener =  tokio::net::TcpListener::bind(addres).await.unwrap();
 
     //3. start the server
-    axum::serve(listener, router1).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
 
--- src/vehicle.rs
+++ src/vehicle.rs
@@ -2,7 +2,7 @@ use axum::{Json};
 
 #[derive(Debug, serde::Serialize)]
 pub struct Vehicle {
-    id: String,
+    id: Option<String>,
     name: String,
     model: String,
     manufacturer: String,
@@ -15,7 +15,7 @@ pub async fn vehicle_get() -> Json<Vehicle> {
     println!("Received a GET request to /vehicle");
     Json::from(
         Vehicle {
-        id: uuid::Uuid::new_v4().to_string(),
+        id: Some(uuid::Uuid::new_v4().to_string()),
         name: "Koenigsegg".to_string(),
         model: "Agera RS".to_string(),
         manufacturer: "Koenigsegg Automotive AB".to_string(),
@@ -26,7 +26,13 @@ pub async fn vehicle_get() -> Json<Vehicle> {
     
 }
 
-pub async fn vehicle_get_post() {
-    // handle post request
-    println!("Received a POST request to /vehicle");
+pub async fn vehicle_get_post(Json(mut v): Json<Vehicle>) {
+   //println!("Received a POST request to /vehicle with payload: {:?}", v);
+    println!("name{0}, model{1}, manufacturer{2}, year{3}, color{4}", v.name, v.model, v.manufacturer, v.year, v.color);
+
+    v.id = Some(uuid::Uuid::new_v4().to_string());
+    println!("Vehicle created with ID: {:?}", v.id);
+    Json::from(v);
+
+
 }
