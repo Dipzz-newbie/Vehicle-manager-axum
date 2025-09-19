90523727d49907f50b033a0ea2748fac6652acf4
Dipzz-newbie <mnadhiftriyudo@gmail.com>
2025-09-19 18:11:48 +0700

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Json, Router};
 
 #[tokio::main]
 async fn main() {
@@ -13,10 +13,30 @@ async fn main() {
     axum::serve(listener, router1).await.unwrap();
 }
 
+#[derive(Debug, serde::Serialize)]
+struct Vehicle {
+    id: String,
+    name: String,
+    model: String,
+    manufacturer: String,
+    year: u16,
+    color: String,
+}
 
-async fn vehicle_get() {
-    // handle get request
-    println!("Received a GET request to /vehicle");
+
+async fn vehicle_get() -> Json<Vehicle> {
+    
+    Json::from(
+        Vehicle {
+        id: uuid::Uuid::new_v4().to_string(),
+        name: "Koenigsegg".to_string(),
+        model: "Agera RS".to_string(),
+        manufacturer: "Koenigsegg Automotive AB".to_string(),
+        year: 2015,
+        color: "Red On Black".to_string(),
+
+    })
+    
 }
 
 async fn vehicle_get_post() {
